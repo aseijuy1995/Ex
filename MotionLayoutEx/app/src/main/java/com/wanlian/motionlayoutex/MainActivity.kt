ae616452d17ff5389030754490e721429670c5be
Yujie <b0972911675@gmail.com>
2020-11-03 18:21:09 +0800

    Add MotionLayoutEx

---
+++ MotionLayoutEx/app/src/main/java/com/wanlian/motionlayoutex/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.wanlian.motionlayoutex
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main2)
+//        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main4)
+    }
+}
