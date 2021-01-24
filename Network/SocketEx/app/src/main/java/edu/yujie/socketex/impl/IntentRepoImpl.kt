510035fbeb5db98bb9b82ed548a7eaa9d8d1c1b4
YuJie <b0972911675@gmail.com>
2021-01-25 03:15:54 +0800

    Fix

--- Network/SocketEx/app/src/main/java/edu/yujie/socketex/impl/IntentRepoImpl.kt
+++ Network/SocketEx/app/src/main/java/edu/yujie/socketex/impl/IntentRepoImpl.kt
@@ -16,7 +16,7 @@ import org.koin.core.KoinComponent
 import org.koin.core.inject
 
 
-class MediaRepo2Impl : IMediaRepo2(), KoinComponent {
+class IntentRepoImpl : IIntentRepo(), KoinComponent {
     private val camera by inject<ICamera>()
     private val crop by inject<ICrop>()
     private val album by inject<IAlbum>()
@@ -37,28 +37,28 @@ class MediaRepo2Impl : IMediaRepo2(), KoinComponent {
 
     override fun onAlbumResult(result: IntentResult): Observable<IntentResult> = album.onAlbumResult(result = result)
 
-    //recorder
-    /**
-     * Note: On devices running Android 9 (API level 28) or higher, apps running in the background cannot access the microphone.
-     * Therefore, your app should record audio only when it's in the foreground or when you include an instance of MediaRecorder in a foreground service.
-     * */
-    override fun prepareRecording(context: Context): Completable = recorder.prepareRecording(setting = RecorderSetting(context = context))
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-    override val stateRe: BehaviorRelay<Boolean>
-        get() = recorder.stateRelay
-
-    override val lessTimeRelay: BehaviorRelay<Boolean>
-        get() = recorder.lessTimeRelay
-
-
-    override fun startRecording(): Observable<Long> = recorder.startRecording()
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-
-    override fun stopRecording(): Completable = recorder.stopRecording()
-        .subscribeOn(AndroidSchedulers.mainThread())
+//    //recorder
+//    /**
+//     * Note: On devices running Android 9 (API level 28) or higher, apps running in the background cannot access the microphone.
+//     * Therefore, your app should record audio only when it's in the foreground or when you include an instance of MediaRecorder in a foreground service.
+//     * */
+//    override fun prepareRecording(context: Context): Completable = recorder.prepareRecording(setting = RecorderSetting(context = context))
+//        .subscribeOn(Schedulers.io())
+//        .observeOn(AndroidSchedulers.mainThread())
+//
+//    override val stateRe: BehaviorRelay<Boolean>
+//        get() = recorder.stateRelay
+//
+//    override val lessTimeRelay: BehaviorRelay<Boolean>
+//        get() = recorder.lessTimeRelay
+//
+//
+//    override fun startRecording(): Observable<Long> = recorder.startRecording()
+//        .subscribeOn(Schedulers.io())
+//        .observeOn(AndroidSchedulers.mainThread())
+//
+//    override fun stopRecording(): Completable = recorder.stopRecording()
+//        .subscribeOn(AndroidSchedulers.mainThread())
 
 
 }
