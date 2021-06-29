67c1514e6b27edce02f707f3af55804da52820cc
YuJie <b0972911675@gmail.com>
2021-06-30 03:06:39 +0800

    Fix Mode

--- Sample/Coachingapp/app/src/main/java/tw/north27/coachingapp/ui/launch/LaunchActivity.kt
+++ Sample/Coachingapp/app/src/main/java/tw/north27/coachingapp/ui/launch/LaunchActivity.kt
@@ -5,9 +5,10 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowInsets
-import android.view.WindowInsetsController
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.yujie.utilmodule.adapter.bindImg
@@ -46,47 +47,25 @@ class LaunchActivity : BaseAppCompatActivity<ActivityLaunchBinding>(ActivityLaun
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             WindowCompat.setDecorFitsSystemWindows(window, false)
-            val controller = ViewCompat.getWindowInsetsController(window.decorView)
-            window.insetsController?.let {
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            val control = ViewCompat.getWindowInsetsController(window.decorView)
+            control?.apply {
+                systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                show(WindowInsetsCompat.Type.systemBars())
+                hide(WindowInsets.Type.navigationBars())
             }
-            controller?.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
-            controller?.isAppearanceLightStatusBars = false
-            controller?.isAppearanceLightNavigationBars = false
         } else {
-//            window.apply {
-//                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-//                addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-////                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-//                statusBarColor = Color.TRANSPARENT
-////                setStatusBarColor()
-//                navigationBarColor = Color.TRANSPARENT
-//            }
-
             window.apply {
                 decorView.systemUiVisibility =
 //                    View.SYSTEM_UI_FLAG_FULLSCREEN or //隱藏狀態欄
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or  //隱藏導航欄
                             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or //粘性沉浸模式
-
-//                    View.SYSTEM_UI_FLAG_LOW_PROFILE or
                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or //內容顯示在狀態欄後面
                             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION //內容顯示在導航欄後面
-
-
-                statusBarColor = Color.TRANSPARENT
-                navigationBarColor = Color.TRANSPARENT
-                //
-                //                addFlags(WindowCompat.FEATURE_ACTION_BAR_OVERLAY)
-//                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-//                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-//                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-//                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-
-
             }
         }
     }
